4316ead3a427e8ffdc3c29e454268e4dc80cfd06
linqitai <514917919@qq.com>
2020-01-25 20:53:42 +0800

    lqt

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,26 @@
 module.exports = {
-    // baseUrl: './',
+	configureWebpack: (config) => {
+		if (process.env.NODE_ENV === 'production') {
+			config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+		}
+	},
+	// baseUrl: './',
 	publicPath: './',
-    productionSourceMap: false,
-    devServer: {
+	productionSourceMap: false,
+	devServer: {
 		port: 8086,
-        proxy: {
-            '/bflApp':{
-                target:'http://localhost:8001/',
-                changeOrigin:true,
-                pathRewrite:{
-                    '/bflApp':''
-                }
-            },
-            /* '/ms':{
-                target: 'https://www.easy-mock.com/mock/592501a391470c0ac1fab128',
-                changeOrigin: true
-            } */
-        }
-    }
+		proxy: {
+			'/bflApp': {
+				target: 'http://localhost:8001/',
+				changeOrigin: true,
+				pathRewrite: {
+					'/bflApp': ''
+				}
+			},
+			/* '/ms':{
+			    target: 'https://www.easy-mock.com/mock/592501a391470c0ac1fab128',
+			    changeOrigin: true
+			} */
+		}
+	}
 }
